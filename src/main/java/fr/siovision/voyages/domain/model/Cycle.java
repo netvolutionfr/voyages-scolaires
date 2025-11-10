cb73cfca0ece954aefd85b3d1d1c3d4e11bb0ed7
Stanis <stanis@netvolution.fr>
2025-11-10 21:12:02 +0100

    Update educational cycles and sections in database schema

--- src/main/java/fr/siovision/voyages/domain/model/Cycle.java
+++ src/main/java/fr/siovision/voyages/domain/model/Cycle.java
@@ -1,11 +1,10 @@
 package fr.siovision.voyages.domain.model;
 
 public enum Cycle {
-    COLLEGE,
-    LYCEE_GENERAL,
-    LYCEE_TECHNO,
-    LYCEE_PRO,
     BTS,
-    ALTERNANCE,
-    PREPA
+    FC,
+    LYCEE_GENERAL,
+    LYCEE_PRO,
+    PREPA,
+    SUPERIEUR
 }
--- src/main/java/fr/siovision/voyages/domain/model/YearTag.java
+++ src/main/java/fr/siovision/voyages/domain/model/YearTag.java
@@ -2,14 +2,17 @@ package fr.siovision.voyages.domain.model;
 
 /** Ordre pédagogique : 3e < 2nde < 1ere < Tle < BTS1 < BTS2 < B3 < B4 < B5 */
 public enum YearTag {
-    TROISIEME,
+    TROISIEME_PRO,
+    LYCEE_PRO,
     SECONDE,
+    SECONDE_PRO,
     PREMIERE,
+    PREMIERE_PRO,
     TERMINALE,
+    TERMINALE_PRO,
     BTS1,
     BTS2,
-    B3,
-    B4,
-    B5,
-    PREPA
+    FC,
+    PREPA,
+    SUPERIEUR,
 }
